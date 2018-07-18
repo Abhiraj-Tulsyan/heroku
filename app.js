4d9237b026a30a589a2fffbec3a18273d1bd7237
AbhirajTulsyan <abhituls55@gmail.com>
2018-07-18 18:34:12 +0530

    messge

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express=require('express');
 var request = require('request');
 var app=express();
+var path =require('path');
+console.log(__dirname);
 var bodyParser=require('body-parser');
 var port = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
